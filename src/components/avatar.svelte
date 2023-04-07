64e5b83390d108e08b194cf6854934479c079a95
huvber <huvber@gmail.com>
2023-04-07 10:26:54 +0200

    chore: move the repo to sourcehut

--- src/components/avatar.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-  export let url = '';
-  export let name = '';
-  export let size: 'small' | 'medium' | 'large' = 'large';
-</script>
-
-<img class={`nes-avatar is-rounded is-${size}`} src={url} alt={name} />
--- src/routes/login/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-  import Button from '../../components/button.svelte';
-  import { signIn } from '../../modules/auth';
-</script>
-
-<h1>This is Login</h1>
-<Button on:click={signIn}>Login</Button>
